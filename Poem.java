36ead07a632fd1ff1e02518c7aef5fee371ce59c
SowmyaDondeti <46711932+SowmyaDondeti@users.noreply.github.com>
2019-01-25 13:01:25 -0600

    Poem practice program

---
+++ Poem.java
@@ -0,0 +1,40 @@
+public class Poem {
+    public static void main(String[] args){
+        System.out.println("JABBERWOCKY \n");
+        System.out.println("'Twas brillig, and the slithy toves");
+        System.out.println("   Did gyre and gimble in the wabe;");
+        System.out.println("All mimsy were the borogoves,");
+        System.out.println("   And the momo raths outgrabe.\n");
+        System.out.println("\"Beware the Jabberwock, my son!");
+        System.out.println("   The jaws that bite, the claws that catch!");
+        System.out.println("Beware the Jubjub bird, and shun");
+        System.out.println("   The frumious Bandersnatch!\"\n");
+        System.out.println("He took his vorpal sword in hand:\n   Long time the manxome foe he sought -\nSo rested he by the Tumtum tree,\n   And stood awhile in thought.\n");
+        System.out.println("And as in uffish thought he stood,\n" +
+                "   The Jabberwock, with eyes of flame,\n" +
+                "Came whiffling through the tulgey wood,\n" +
+                "   And burbled as it came!");
+        System.out.println("One, two! One, two! And through and through\n" +
+                "   The vorpal blade went snicker-snack!\n" +
+                "He left it dead, and with its head\n" +
+                "   He went galumphing back.\n");
+        System.out.println("\"And hast thou slain the Jabberwock?\n" +
+                "   Come to my arms, my beamish boy!\n" +
+                "O frabjous day! Callooh! Callay!\"\n" +
+                "   He chortled in his joy.\n");
+        System.out.println("'Twas brillig, and the slithy toves\n" +
+                "   Did gyre and gimble in the wabe;\n" +
+                "All mimsy were the borogoves,\n" +
+                "   And the mome raths outgrabe.\n");
+        System.out.println("\"It seems very pretty,\" she said when she had finished\n" +
+                "it, \"but it's RATHER hard to understand!\" (You see\n" +
+                "she didn't like to confess, ever to herself, that\n" +
+                "she couldn't make it out at all.) \"Somehow it seems\n" +
+                "to fill my head with ideas - only I don't exactly\n" +
+                "know what they are! However, SOMEBODY killed\n" +
+                "SOMETHING: that's clear, at any rate.\"");
+
+
+
+    }
+}
