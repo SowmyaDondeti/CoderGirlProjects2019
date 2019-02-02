362d54ab3ae2a17f81b379634cf8e55449caae0a
SowmyaDondeti <46711932+SowmyaDondeti@users.noreply.github.com>
2019-02-02 07:06:16 -0600

    Add files via upload

---
+++ SandwichShop.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class SandwichShop
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        int goalForVeggies = 50;
+        int goalForBurgers = 250;
+        int goalForSubs = 180;
+        int goalForSoup = 70;
+        int veggiesSold, burgersSold, subsSold, soupSold;
+        System.out.print("How many veggie sandwiches are sold today?");
+        veggiesSold = keyboard.nextInt();
+        if (veggiesSold >= goalForVeggies)
+        {
+            System.out.println("Meets goal");
+        }
+        else
+        {
+            System.out.println("Fell short");
+        }
+        System.out.print("How many burgers are sold today? ");
+        burgersSold = keyboard.nextInt();
+        if (burgersSold >= goalForBurgers)
+        {
+            System.out.println("Meets goal");
+        }
+        else
+        {
+            System.out.println("Fell short");
+        }
+        System.out.print("How many subs are sold today?");
+        subsSold = keyboard.nextInt();
+        if (subsSold >= goalForSubs)
+        {
+            System.out.println("Meets goal");
+        }
+        else
+        {
+            System.out.println("Fell short");
+        }
+        System.out.print("How many soups are sold today?");
+        soupSold = keyboard.nextInt();
+        if (soupSold >= goalForSoup)
+        {
+            System.out.println("Meets goal");
+        }
+        else
+        {
+            System.out.println("Fell short");
+        }
+
+        if (veggiesSold >= goalForVeggies
+                && burgersSold >= goalForBurgers
+                && subsSold >= goalForSubs
+                && soupSold >= goalForSoup)
+        {
+            System.out.println("Made goal for everything!");
+        }
+    }
+}
